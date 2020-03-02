60f3c7c845af635f518cc30ec4063df1e39505b6
Luzzi Charly <luzzi.charly@gmail.com>
2020-03-02 11:18:16 +0100

    'init2'

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,8 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <ion-app class="backgroundImg">
+      <ion-vue-router />
+    </ion-app>
   </div>
 </template>
 
@@ -29,4 +27,7 @@
     }
   }
 }
+  .backgroundImg {
+    --ion-background-color: #5c91ff;
+  }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,140 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <ion-app>
+    <ion-split-pane when="sm">
+      <ion-menu>
+        <ion-header>
+          <ion-toolbar color="primary">
+            <ion-title>Menu</ion-title>
+          </ion-toolbar>
+        </ion-header>
+
+        <ion-content>
+          <ion-list>
+            <ion-list-header>
+              Navigate
+            </ion-list-header>
+            <ion-menu-toggle auto-hide="false">
+              <ion-item button>
+                <ion-icon slot="start" name='home'></ion-icon>
+                <ion-label>
+                  Home
+                </ion-label>
+              </ion-item>
+            </ion-menu-toggle>
+          </ion-list>
+        </ion-content>
+      </ion-menu>
+
+      <div class="ion-page" main>
+        <ion-header>
+          <ion-toolbar>
+            <ion-buttons slot="start">
+              <ion-menu-toggle>
+                <ion-button>
+                  <ion-icon slot="icon-only" name="menu"></ion-icon>
+                </ion-button>
+              </ion-menu-toggle>
+            </ion-buttons>
+            <ion-title>{{!this.search ? 'Paris' : this.search}}</ion-title>
+          </ion-toolbar>
+        </ion-header>
+        <ion-content class="ion-padding">
+
+          <ion-item>
+            <ion-label position="floating">Rechercher une ville...</ion-label>
+            <ion-input :value="search" @input="search = $event.target.value" v-on:keyup.enter="getWeather"></ion-input>
+          </ion-item>
+
+          <ion-grid>
+            <ion-row>
+              <ion-col></ion-col>
+              <ion-col>
+                <div>
+                  <ion-icon name="cloudy"></ion-icon>
+                </div>
+              </ion-col>
+              <ion-col>
+                <div>
+                  {{currentTemp}} °C
+                </div>
+              </ion-col>
+              <ion-col></ion-col>
+            </ion-row>
+          </ion-grid>
+
+          <ion-list>
+            <ion-item>
+              <ion-label>Température : Min {{minTemp}} °C <br> Max {{maxTemp}} °C</ion-label>
+            </ion-item>
+            <ion-item>
+              <ion-label>Lever du soleil : {{sunrise}}</ion-label>
+            </ion-item>
+            <ion-item>
+              <ion-label>Coucher du soleil : {{sunset}}</ion-label>
+            </ion-item>
+            <ion-item>
+              <ion-label>Humidité : {{humidity}}</ion-label>
+            </ion-item>
+            <ion-item>
+              <ion-label>Pression atmosphérique : {{pressure}}</ion-label>
+            </ion-item>
+            <ion-item>
+              <ion-label>Vent : {{wind}}</ion-label>
+            </ion-item>
+          </ion-list>
+        </ion-content>
+      </div>
+    </ion-split-pane>
+  </ion-app>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+  import axios from 'axios'
+  export default {
+    name: "home",
+    data () {
+      return {
+        search: null,
 
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+        currentTemp: '',
+        minTemp: '',
+        maxTemp:'',
+        sunrise: '',
+        sunset: '',
+        pressure: '',
+        humidity: '',
+        wind: '',
+        overcast: '',
+        icon: ''
+      }
+    },
+    methods: {
+      getWeather () {
+        let rqst = !this.search ? 'paris' : this.search
+        let url = 'http://api.openweathermap.org/data/2.5/weather?q=' + rqst + '&units=metric&APPID=' + process.env.VUE_APP_OPEN_WEATHER;
+        axios.get(url)
+            .then(response => {
+              console.log(response.data);
+              this.currentTemp = response.data.main.temp;
+              this.minTemp = response.data.main.temp_min;
+              this.maxTemp = response.data.main.temp_max;
+              this.pressure = response.data.main.pressure;
+              this.humidity = response.data.main.humidity + '%';
+              this.wind = response.data.wind.speed + 'm/s';
+              this.overcast = response.data.weather[0].description;
+              this.icon = "images/" + response.data.weather[0].icon.slice(0, 2) + ".svg";
+              this.sunrise = new Date(response.data.sys.sunrise*1000).toLocaleTimeString("en-GB").slice(0,4);
+              this.sunset = new Date(response.data.sys.sunset*1000).toLocaleTimeString("en-GB").slice(0,4);
+
+            })
+            .catch(error => {
+              console.log(error);
+            });
+
+      }
+    },
+    beforeMount() {
+      this.getWeather('paris');
+    },
+  };
 </script>
